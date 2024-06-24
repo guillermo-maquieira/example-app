41807372292f329d44f9341a22c5a67c9ccf937b
Guillermo Maquieira <guimaq@gmail.com>
2024-06-24 10:41:11 -0300

    Faker & Seeder

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        foreach (range(1,3) as $index){
+            DB::table('contacts')->insert([
+                'id' => $faker->randomNumber(),
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'email' => Str::random(10).'@email.com',
+            ]);
+        }
+    }
+}
